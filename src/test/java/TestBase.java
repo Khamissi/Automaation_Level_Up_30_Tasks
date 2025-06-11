9ed89a9c4dbd399849cc3bfa987ce05adbe5fca2
Ahmed Elkhamissi <12422021668190@pg.cu.edu.eg>
2025-06-11 15:25:33 +0300

    Add tasks 6 and XML file

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 
 public class TestBase
@@ -12,20 +14,22 @@ public class TestBase
     protected WebDriver driver;
 
    @BeforeMethod
-    public void setUpChrome()
+   @Parameters({"browser"})
+   public void setUp(@Optional("chrome") String browserName)
     {
-        ChromeOptions options;
-        options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        driver = new ChromeDriver(options);
-    }
-
-    @BeforeMethod
-    public void setUpMozilla()
-    {
-        FirefoxOptions options;
-        options = new FirefoxOptions();
-        driver = new FirefoxDriver(options);
+        if (browserName.equals("chrome"))
+        {
+            ChromeOptions options;
+            options = new ChromeOptions();
+            options.addArguments("start-maximized");
+            driver = new ChromeDriver(options);
+        }
+        else if (browserName.equals("firefox"))
+        {
+            FirefoxOptions options;
+            options = new FirefoxOptions();
+            driver = new FirefoxDriver(options);
+        }
     }
 
     @AfterMethod
